7229c1c8e1a20b65fd3e9037c72f195d73be8d99
Nick Coghlan <ncoghlan@gmail.com>
2017-03-31 18:59:31 +1000

    Prompt for sudo before running scenarios

--- integration-tests/features/environment.py
+++ integration-tests/features/environment.py
@@ -1,4 +1,11 @@
 import contextlib
+import subprocess
+
+def before_all(context):
+    # Some steps require sudo, so for convenience in interactive use,
+    # we ensure we prompt for elevated permissions immediately,
+    # rather than potentially halting midway through a test
+    subprocess.check_output(["sudo", "echo", "Elevated permissions needed"])
 
 def before_scenario(context, scenario):
     context.resource_manager = contextlib.ExitStack()
