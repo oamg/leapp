8997f4b4a9be295b08cb166155f243ac995d93e7
Pavel Odvody <shaded-enmity@users.noreply.github.com>
2017-04-19 00:53:34 +0200

    Update bootstrap.sh
    Install pip first

--- centos-ci/bootstrap.sh
+++ centos-ci/bootstrap.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 if [[ $(id -u) != 0 ]]; then
    echo "$0 needs to be run as root" ; exit 1
 fi
+
+yum install -y http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-9.noarch.rpm
+yum install -y python2-pip
 
 pip install ansible==2.2.0
 
