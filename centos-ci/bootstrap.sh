df56b00784ad7cd461486dcdc989f014e2ddcc22
Vinzenz Feenstra <vfeenstr@redhat.com>
2017-05-03 08:44:49 +0200

    ci-bootstrap: Don't fail on things already installed

--- centos-ci/bootstrap.sh
+++ centos-ci/bootstrap.sh
@@ -6,7 +6,7 @@ if [[ $(id -u) != 0 ]]; then
    echo "$0 needs to be run as root" ; exit 1
 fi
 
-yum install -y http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-9.noarch.rpm
+yum install -y http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-9.noarch.rpm || :
 yum install -y python2-pip gcc redhat-rpm-config openssl-devel python-devel
 
 pip install ansible==2.2.0
