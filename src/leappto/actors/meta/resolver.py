3bb5842ababa20b46409b557fba597d0e3872ef8
Vinzenz Feenstra <vfeenstr@redhat.com>
2017-05-18 09:28:52 +0200

    Fix meta data resolver

--- src/leappto/actors/meta/resolver.py
+++ src/leappto/actors/meta/resolver.py
@@ -1,11 +1,12 @@
 from . import registry, _fq
 
+
 def resolve(services):
     lookup = {_fq(s): s.leapp_meta() for s in services}
     for s in services:
         m = lookup[_fq(s)]
         for link in  m.get('address_links', []):
-            tgtm = lookup[link['target']].leapp_meta()
+            tgtm = lookup[link['target']]
             tgtm['fixup'] = tgtm.get('fixup', []) + [(s, link)]
-            tgtm['require_links'] = tgtm.get('require_links', []) + [_fq(s)]
+            tgtm['require_links'] = tgtm.get('require_links', []) + [{'target': _fq(s)}]
 
