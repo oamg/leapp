068b1d6440ba1fe944c601f282657972730b93d6
Vinzenz Feenstra <vfeenstr@redhat.com>
2017-05-18 09:29:08 +0200

    Add dependency ordering

--- src/leappto/actors/meta/resolver.py
+++ src/leappto/actors/meta/resolver.py
@@ -10,3 +10,23 @@ def resolve(services):
             tgtm['fixup'] = tgtm.get('fixup', []) + [(s, link)]
             tgtm['require_links'] = tgtm.get('require_links', []) + [{'target': _fq(s)}]
 
+
+def dependency_ordered(services):
+    result = []
+    result_fq = []
+    for s in services:
+        if not s.leapp_meta().get('require_links', []):
+            result.append(s)
+            result_fq.append(_fq(s))
+
+    services = [s for s in services if s not in result]
+    count = len(services)
+    while services and count > 0:
+        count -= 1
+        for s in services:
+            if all([link['target'] in result_fq for link in s.leapp_meta().get('require_links', [])]):
+                result.append(s)
+                result_fq.append(_fq(s))
+        services = [s for s in services if s not in result]
+    result += services
+    return result
