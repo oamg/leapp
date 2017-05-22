a113854941ce09e4dabc53534adcdac58c8539bd
Marcel Gazdik <mgazdik@redhat.com>
2017-05-22 15:52:20 +0200

    Changed project name Leapp -> LeApp-To

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,7 +48,7 @@ source_suffix = ['.rst', '.md']
 master_doc = 'index'
 
 # General information about the project.
-project = 'Leapp'
+project = 'LeApp-To'
 copyright = '2017, Leapp Team'
 author = 'Leapp Team'
 
