b989297d3afc708146e3f5b075ee3357b696865d
GAZDOWN <root@mgazdik-leapp.usersys.redhat.com>
2017-05-31 17:35:30 +0200

    added port override test

--- integration-tests/features/default-port-detection.feature
+++ integration-tests/features/default-port-detection.feature
@@ -20,3 +20,10 @@ Scenario: Return default port detected on source machine - add port 11111
          | source     | centos6-guest-httpd | no           |
          | target     | centos7-target      | no           |
      Then get list of discovered ports on source which will be forwarded from target and add port 11111 to 11111
+
+Scenario: Disable default port mapping and return user defined ports - port 11111, port 11112
+   Given the local virtual machines:
+         | name       | definition          | ensure_fresh |
+         | source     | centos6-guest-httpd | no           |
+         | target     | centos7-target      | no           |
+     Then get list of user defined ports from source which will be forwarded from target - port 11111, port 11112
--- integration-tests/features/steps/default-port-detection.py
+++ integration-tests/features/steps/default-port-detection.py
@@ -43,7 +43,7 @@ def check_specific_ports_used_by_vm_with_override(context, vm_source_name, vm_ta
     _assert_discovered_ports(ports, expected_ports)
 
 @then("get list of discovered ports on {vm_source_name} which will be forwarded from {vm_target_name} and add port {source_port} to {target_port}")
-def check_specific_ports_used_by_vm_with_override(context, vm_source_name, vm_target_name, source_port, target_port):
+def check_specific_ports_used_by_vm_with_addition(context, vm_source_name, vm_target_name, source_port, target_port):
     """check if ports 22,80,111,8080 are open and forwarded to 9022,8080,111,8080"""
 
     ports = context.cli_helper.check_response_time(
@@ -51,4 +51,16 @@ def check_specific_ports_used_by_vm_with_override(context, vm_source_name, vm_ta
         time_limit=60
     )
     expected_ports = "[[22,9022],[80,80],[111,111],[{},{}]]".format(source_port, target_port)
+    _assert_discovered_ports(ports, expected_ports)
+
+@then("get list of user defined ports from {vm_source_name} which will be forwarded from {vm_target_name} - port {target_port_0}, port {target_port_1}")
+def check_user_defined_ports(context, vm_source_name, vm_target_name, target_port_0, target_port_1):
+    """check if ports 22,80,111,8080 are open and forwarded to 9022,8080,111,8080"""
+
+    ports = context.cli_helper.check_response_time(
+        ["migrate-machine", "-p", "--ignore-default-port-map", "-t", _get_hostname(context, vm_target_name), _get_hostname(context, vm_source_name), "--tcp-port", "{}:{}".format(target_port_0, target_port_0), "{}:{}".format(target_port_1, target_port_1)],
+        time_limit=60
+    )
+
+    expected_ports = "[[{},{}],[{},{}]]".format(target_port_0, target_port_0, target_port_1, target_port_1)
     _assert_discovered_ports(ports, expected_ports)
