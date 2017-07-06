07ef17c954f8b38cb9d63ba5aacb3dbb1fdc0042
GAZDOWN <mgazdik@redhat.com>
2017-07-06 10:28:29 +0200

    Add: ansible boolean container_manage_cgroup, better comments

--- src/leappto/cli.py
+++ src/leappto/cli.py
@@ -756,8 +756,10 @@ def main():
                 
             ]
 
-            result = mc.start_container('centos/systemd',
-                                        #'/usr/lib/systemd/systemd --system',
+            # centos/systemd doesn't have any other tag than latest
+            # but it is derived from centos:7 image
+            result = mc.start_container('centos/systemd:latest',
+                                        # This is not relict from CentOS6, it will execute systemd properly 
                                         '/sbin/init',
                                         tcp_mapping,
                                         " ".join(opts))
