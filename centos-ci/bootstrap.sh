427b0d3074914e924e6959ed304b185ae44c870d
Vinzenz Feenstra <vfeenstr@redhat.com>
2017-07-19 08:41:45 +0200

    integration-tests: Fix new epel release number

--- centos-ci/bootstrap.sh
+++ centos-ci/bootstrap.sh
@@ -20,7 +20,7 @@ if [[ $(id -u) != 0 ]]; then
    echo "$0 needs to be run as root" ; exit 1
 fi
 
-yum install -y http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-9.noarch.rpm
+yum install -y http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-10.noarch.rpm
 yum install -y python2-pip gcc redhat-rpm-config openssl-devel python-devel
 
 # Interim EPEL-based approach to enable testing of --ask-pass option
