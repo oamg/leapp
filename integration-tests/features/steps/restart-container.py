665181a72f5d920fe38564747685385e85bbbe85
Vinzenz Feenstra <vfeenstr@redhat.com>
2017-07-24 13:53:35 +0200

    integration-tests: Fixed signature of restart container tests

--- integration-tests/features/steps/restart-container.py
+++ integration-tests/features/steps/restart-container.py
@@ -4,10 +4,10 @@ from behave import when
 
 
 @then("container named {container_name} should be restarted and running")
-def get_container_status():
+def get_container_status(context, container_name):
     pass
 
 
 @when("docker is restarted")
-def restart_docker():
+def restart_docker(context):
     pass
