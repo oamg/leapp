ca63f120c11564cb0e0e64b5b0e62bf6a744b34a
Vinzenz Feenstra <vfeenstr@redhat.com>
2018-01-31 11:39:57 +0100

    packaging: Some more fixes required to make the SRPM building more robust

--- .copr/build.sh
+++ .copr/build.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-BRANCH=master
-LEAPP_PATCHES_SINCE_RELEASE="$(git log `git describe  --abbrev=0`..HEAD --format=oneline | wc -l)"
-
 export
 if [ -z "$(which git)" ]; then
     dnf -y install git-core
 fi
+
+if [ -n "$1" ]
+then
+    pushd $1
+fi
+
+BRANCH=master
+LEAPP_PATCHES_SINCE_RELEASE="$(git log `git describe  --abbrev=0`..HEAD --format=oneline | wc -l)"
+echo LEAPP_PATCHES_SINCE_RELEASE=$LEAPP_PATCHES_SINCE_RELEASE
+
 
 rm -rf leapp-build
 mkdir -p leapp-build
@@ -15,18 +22,21 @@ pushd leapp-build
 git clone --depth=1000 https://github.com/leapp-to/leapp-actors
     pushd leapp-actors
     LEAPP_ACTORS_PATCHES_SINCE_RELEASE="$(git log `git describe  --abbrev=0`..HEAD --format=oneline | wc -l)"
+    echo LEAPP_ACTORS_PATCHES_SINCE_RELEASE=$LEAPP_ACTORS_PATCHES_SINCE_RELEASE
     rm -rf .git
     popd
 
 git clone --depth=1000 https://github.com/leapp-to/leappctl
     pushd leappctl
     LEAPPCTL_PATCHES_SINCE_RELEASE="$(git log `git describe  --abbrev=0`..HEAD --format=oneline | wc -l)"
+    echo LEAPPCTL_PATCHES_SINCE_RELEASE=$LEAPPCTL_PATCHES_SINCE_RELEASE
     rm -rf .git
     popd
 
 git clone --depth=1000 https://github.com/leapp-to/snactor
     pushd snactor
     SNACTOR_PATCHES_SINCE_RELEASE="$(git log `git describe  --abbrev=0`..HEAD --format=oneline | wc -l)"
+    echo SNACTOR_PATCHES_SINCE_RELEASE=$SNACTOR_PATCHES_SINCE_RELEASE
     rm -f python-snactor.spec
     rm -rf .git
     popd
@@ -34,6 +44,7 @@ git clone --depth=1000 https://github.com/leapp-to/snactor
 git clone --depth=1000 https://github.com/leapp-to/leapp-go
     pushd leapp-go
     LEAPP_GO_PATCHES_SINCE_RELEASE="$(git log `git describe  --abbrev=0`..HEAD --format=oneline | wc -l)"
+    echo LEAPP_GO_PATCHES_SINCE_RELEASE=$LEAPP_GO_PATCHES_SINCE_RELEASE
     rm -rf .git
     popd
 
@@ -43,7 +54,9 @@ VERSION=$(git describe  --abbrev=0|cut -d- -f 2)
 DIST=$(git describe  --abbrev=0|cut -d- -f 3)
 
 PATCHES_SINCE_RELEASE=$(($LEAPP_PATCHES_SINCE_RELEASE + $LEAPP_ACTORS_PATCHES_SINCE_RELEASE + $LEAPPCTL_PATCHES_SINCE_RELEASE + $SNACTOR_PATCHES_SINCE_RELEASE + $LEAPP_GO_PATCHES_SINCE_RELEASE))
+echo PATCHES_SINCE_RELEASE=$PATCHES_SINCE_RELEASE
 LEAPP_BUILD_TAG=".$DIST.$(date  --rfc-3339=date | tr -d '-').git.$PATCHES_SINCE_RELEASE"
+echo LEAPP_BUILD_TAG=$LEAPP_BUILD_TAG
 
 /bin/cp ../leapp.spec .
 tar czf leapp-build.tar.gz leapp-build leapp.spec
@@ -56,3 +69,7 @@ fi
 
 rpmbuild --define "_srcrpmdir $SRPMDIR" --define "version $VERSION" --define "dist $LEAPP_BUILD_TAG" -ts ./leapp-build.tar.gz
 
+if [ -n "$1" ]
+then
+    popd
+fi
