4a8f9bc1510bb55eacfdd73723550e5a8cf2144c
Vinzenz Feenstra <vfeenstr@redhat.com>
2018-04-20 12:39:17 +0200

    `leapp upgrade`: load repos, then load and run IPU workflow

--- leapp/cli/upgrade/__init__.py
+++ leapp/cli/upgrade/__init__.py
@@ -1,7 +1,26 @@
 from leapp.utils.clicmd import command, command_opt
+from leapp.repository.scan import find_and_scan_repositories
+from leapp.config import get_config
+from leapp.logger import configure_logger
+
+
+def load_repositories_from(name, repo_path, manager=None):
+    if get_config().has_option('repositories', name):
+        repo_path = get_config().get('repositories', name)
+    return find_and_scan_repositories(repo_path, manager=manager)
+
+
+def load_repositories():
+    manager = load_repositories_from('global_repo_path', '/usr/share/leapp-repostories/repos', manager=None)
+    load_repositories_from('custom_repo_path', '/etc/leapp/repos.d/', manager=manager)
+    manager.load()
+    return manager
 
 
 @command('upgrade', help='')
 @command_opt('resume', is_flag=True, help='Continue the last execution after it was stopped (e.g. after reboot)')
 def upgrade(args):
-    pass
+    configure_logger()
+    repositories = load_repositories()
+    workflow = repositories.lookup_workflow('IPUWorkflow')
+    workflow.run()
