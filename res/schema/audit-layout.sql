da0e123432af06c263024276b4581c1d5ce60588
Vinzenz Feenstra <vfeenstr@redhat.com>
2018-04-20 12:35:39 +0200

    Reformatted SQL schema

--- res/schema/audit-layout.sql
+++ res/schema/audit-layout.sql
@@ -59,19 +59,18 @@ CREATE VIEW IF NOT EXISTS messages_data AS
     message.context   AS context,
     message.stamp     AS stamp,
     message.topic     AS topic,
-    message.type      as type,
-    data_source.actor as actor,
-    data_source.phase as phase,
-    msg_data.hash     as message_hash,
-    msg_data.data     as message_data,
-    host.hostname     as hostname
+    message.type      AS type,
+    data_source.actor AS actor,
+    data_source.phase AS phase,
+    msg_data.hash     AS message_hash,
+    msg_data.data     AS message_data,
+    host.hostname     AS hostname
   FROM
     message
-    JOIN
-    data_source ON data_source.id = message.data_source_id
-    ,
-    message_data as msg_data ON message.message_data_hash = msg_data.hash,
-  host ON host.id = data_source.host_id;
-
+  JOIN
+    data_source              ON data_source.id            = message.data_source_id,
+    message_data AS msg_data ON message.message_data_hash = msg_data.hash,
+    host                     ON host.id                   = data_source.host_id
+;
 
 COMMIT;
