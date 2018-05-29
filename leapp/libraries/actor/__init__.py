10b35bdcca74bc95fbe503e8de1b1742a4cd0e30
jhornice <jhornice@redhat.com>
2018-05-29 15:16:43 +0200

    Edit text in /libraries/actor/__init__.py

--- leapp/libraries/actor/__init__.py
+++ leapp/libraries/actor/__init__.py
@@ -1,10 +1,10 @@
 """
 :py:mod:`leapp.libraries.actor` represents the import location for private actor libraries that
-are placed in the actor's libraries folder
+are placed in the actor's libraries folder.
 
 
 Example:
-      If you actor has a libraries folder with a python module called module.py you can import it
+      If you actor has a libraries folder with a module.py python module, import it
       from the actor like this::
 
       from leapp.libraries.actor import module
