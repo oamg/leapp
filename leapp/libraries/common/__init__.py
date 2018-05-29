5380df9b01044141352a642b21d46aab57c3dc5c
jhornice <jhornice@redhat.com>
2018-05-29 15:41:15 +0200

    Edit text in /libraries/common/__init__.py

--- leapp/libraries/common/__init__.py
+++ leapp/libraries/common/__init__.py
@@ -1,9 +1,9 @@
 """
-:py:mod:`leapp.libraries.common` represents the import location for shared libraries that
-are placed in the repository's libraries folder
+:py:mod:`leapp.libraries.common` represents an import location for shared libraries that
+are placed in the repository's libraries folder.
 
 Example:
-      If any of the repositories has a libraries folder with a python module called module.py you can import it
+      If any of the repositories has a libraries folder with a module.py python module, import it
       from the actor like this::
 
       from leapp.libraries.common import module
@@ -11,4 +11,4 @@ Example:
 """
 
 LEAPP_BUILTIN_COMMON_INITIALIZED = False
-"""Internal variable to the framework to know if the common libraries have been already initialized"""
+"""Internal variable to the framework to inform if the common libraries have been already initialized"""
