1bb879c3624fd65663a89ac43574dca64b6dfabb
Vinzenz Feenstra <vfeenstr@redhat.com>
2018-06-14 14:45:02 +0200

    Added command line flag for new-model to allow specifying the topic

--- leapp/snactor/commands/new_model.py
+++ leapp/snactor/commands/new_model.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 from leapp.utils.project import requires_project, make_class_name, find_project_basedir
-from leapp.utils.clicmd import command_arg, command
+from leapp.utils.clicmd import command_arg, command_opt, command
 from leapp.exceptions import UsageError
 
 
@@ -17,6 +17,7 @@ https://red.ht/leapp-docs
 
 
 @command('new-model', help='Creates a new model', description=_LONG_DESCRIPTION)
+@command_opt('topic', help='Assigns the given topic to the model', metavar='TopicClassName')
 @command_arg('model-name')
 @requires_project
 def cli(args):
@@ -30,14 +31,19 @@ def cli(args):
     model_path = os.path.join(basedir, model_name.lower() + '.py')
     if os.path.exists(model_path):
         raise UsageError("File already exists: {}".format(model_path))
+    topic_usage = 'None #  TODO: import appropriate topic and set it here'
+    topic_import = ''
+    if args.topic:
+        topic_usage = args.topic
+        topic_import = 'from leapp.topics import {}\n'.format(args.topic)
 
     with open(model_path, 'w') as f:
         f.write('''from leapp.models import Model, fields
-
+{topic_import}
 
 class {model_name}(Model):
-    topic = None #  TODO: import appropriate topic and set it here
-'''.format(model_name=make_class_name(model_name)))
+    topic = {topic_usage}
+'''.format(model_name=make_class_name(model_name), topic_import=topic_import, topic_usage=topic_usage))
 
     sys.stdout.write("New model {} has been created in {}\n".format(make_class_name(model_name),
                                                                     os.path.realpath(model_path)))
