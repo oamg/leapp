d15f5f603b3db7b0b3dfeabbde11e4ff9049a369
Jozef Mikovic <jmikovic@redhat.com>
2018-09-04 10:08:17 +0200

    allow False as a default in component

--- leapp/dialogs/components.py
+++ leapp/dialogs/components.py
@@ -25,7 +25,7 @@ class Component(object):
         self.key = key or type(self).key
         self.label = label or type(self).label
         self.description = description or type(self).description
-        self.default = default or type(self).default
+        self.default = default if default is not None else type(self).default
         self.value = type(self).value
         self.reason = reason or type(self).reason
 
