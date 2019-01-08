55f5471f74f7e6e32e7f870b6f42a119af041435
Vinzenz Feenstra <vfeenstr@redhat.com>
2019-01-08 16:24:13 +0100

    stdlib: check_output should be call
    Signed-off-by: Vinzenz Feenstra <vfeenstr@redhat.com>

--- tests/scripts/test_stdlib.py
+++ tests/scripts/test_stdlib.py
@@ -1,21 +1,21 @@
-from leapp.libraries.stdlib import check_output
+from leapp.libraries.stdlib import call
 
 
 def test_check_single_line_output():
     a_command = ['echo', 'This a single line test!']
-    assert check_output(a_command) == [u'This a single line test!']
+    assert call(a_command) == [u'This a single line test!']
 
 
 def test_check_single_line_output_no_split():
     a_command = ['echo', 'This a single line No Split test!']
-    assert check_output(a_command, split=False) == u'This a single line No Split test!\n'
+    assert call(a_command, split=False) == u'This a single line No Split test!\n'
 
 
 def test_check_multiline_output():
     a_command = ['echo', 'This a multi-\nline test!']
-    assert check_output(a_command) == [u'This a multi-', u'line test!']
+    assert call(a_command) == [u'This a multi-', u'line test!']
 
 
 def test_check_multiline_output_no_split():
     a_command = ['echo', 'This a multi-\nline No Split test!']
-    assert check_output(a_command, split=False) == u'This a multi-\nline No Split test!\n'
+    assert call(a_command, split=False) == u'This a multi-\nline No Split test!\n'
