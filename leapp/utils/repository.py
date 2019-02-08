574036ff11f668b26bca26ac36b4921caf7dc4b2
Jozef Mikovic <jmikovic@redhat.com>
2019-02-08 10:16:24 +0100

    show global repos in snactor repo list

--- leapp/utils/repository.py
+++ leapp/utils/repository.py
@@ -214,7 +214,7 @@ def get_global_repositories_data():
     all_repos = set([os.path.realpath(path) for path in find_repos('/usr/share/leapp-repository') if path.strip()])
     repo_data = {}
     for repo in all_repos:
-        repo_id = get_repository_metadata(repo).get('uuid', None)
+        repo_id = get_repository_metadata(repo).get('id', None)
         if not repo_id:
             continue
         repo_data[repo_id] = {
