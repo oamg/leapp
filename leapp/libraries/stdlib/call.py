8abe21958a287f6a2908e1873e7e04896ce260a5
Pavel Odvody <podvody@redhat.com>
2019-04-11 11:08:06 +0200

    Fix read_fds offset passing and calculation

--- leapp/libraries/stdlib/call.py
+++ leapp/libraries/stdlib/call.py
@@ -36,6 +36,13 @@ def _multiplex(ep, read_fds, callback_raw, callback_linebuffered,
         linebufs = {fd: '' for fd in read_fds}
         decoders = {fd: codecs.getincrementaldecoder(encoding)() for fd in read_fds}
 
+    def _get_fd_type(fd):
+        """ File descriptors passed via `read_fds` are always representing [stdout, stderr],
+            since arrays start at index 0, we need to add 1 to get the real symbolic value
+            `STDOUT` or `STDERR`.
+        """
+        return read_fds.index(fd) + 1
+
     while not ep.closed and len(hupped) != num_expected:
         events = ep.poll(timeout)
         for fd, event in events:
@@ -43,7 +50,7 @@ def _multiplex(ep, read_fds, callback_raw, callback_linebuffered,
                 hupped.add(fd)
                 ep.unregister(fd)
             if event & (select.EPOLLIN | select.EPOLLPRI) != 0:
-                fd_type = read_fds.index(fd) + 1
+                fd_type = _get_fd_type(fd)
                 read = os.read(fd, buffer_size)
                 callback_raw((fd, fd_type), read)
                 if encoding:
@@ -71,7 +78,7 @@ def _multiplex(ep, read_fds, callback_raw, callback_linebuffered,
             if lb:
                 # [stdout, stderr] is relayed, stdout=1 a stderr=2
                 # as the field starting indexed is 0, so the +1 needs to be added
-                callback_linebuffered(read_fds.index(fd) + 1, lb)
+                callback_linebuffered((fd, _get_fd_type(fd)), lb)
 
     return buf
 
