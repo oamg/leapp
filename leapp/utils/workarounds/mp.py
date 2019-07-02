87cb7dbe00df4316edcca769fd12dceed1920b6b
Vinzenz Feenstra <vfeenstr@redhat.com>
2019-07-02 09:02:55 +0200

    workaround: multiprocessing.util.Finalize patched
    Multiprocessing in newer versions received a fix that is not present in
    the el7 version of python. In that fix the Finalize class verifies that
    the callback is supposed to be run in the process that created the
    object. This patch adds a workaround to introduce the same behavior.
    
    Signed-off-by: Vinzenz Feenstra <vfeenstr@redhat.com>

---
+++ leapp/utils/workarounds/mp.py
@@ -0,0 +1,24 @@
+import os
+import multiprocessing.util
+
+
+def apply_workaround():
+    # Implements:
+    # https://github.com/python/cpython/commit/e8a57b98ec8f2b161d4ad68ecc1433c9e3caad57
+    #
+    # Detection of fix: os imported to compare pids, before the fix os has not
+    # been imported
+    if getattr(multiprocessing.util, 'os', None):
+        return
+
+    class FixedFinalize(multiprocessing.util.Finalize):
+        def __init__(self, *args, **kwargs):
+            super(FixedFinalize, self).__init__(*args, **kwargs)
+            self._pid = os.getpid()
+
+        def __call__(self, *args, **kwargs):
+            if self._pid != os.getpid():
+                return None
+            return super(FixedFinalize, self).__call__(*args, **kwargs)
+
+    setattr(multiprocessing.util, 'Finalize', FixedFinalize)
