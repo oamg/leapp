db231c74dd9c6c9efc6f593e96dc5d54983c8932
Petr Stodulka <pstodulk@redhat.com>
2019-12-03 13:15:58 +0100

    libraries.stdlib.run: expose stdin
    Add possibility to setup stdin (string or fd) when run subcommand
    using the libraries.stdlib.run function. That's needed e.g. when
    secret data should be used as input for the command and we do not
    want to expose those data anywhere.
    
    Fixes: #579

--- tests/scripts/test_stdlib.py
+++ tests/scripts/test_stdlib.py
@@ -5,35 +5,60 @@ from leapp.libraries.stdlib import CalledProcessError, run
 from leapp.libraries.stdlib.config import is_debug, is_verbose
 
 
+_STDIN = [[], {}, 0.123, lambda: None]
+
+
 def test_check_single_line_output():
-    a_command = ['echo', 'This a single line test!']
-    assert run(a_command, split=True)['stdout'] == [u'This a single line test!']
+    cmd = ['echo', 'This a single line test!']
+    assert run(cmd, split=True)['stdout'] == [u'This a single line test!']
 
 
 def test_check_single_line_output_no_split():
-    a_command = ['echo', 'This a single line No Split test!']
-    assert run(a_command, split=False)['stdout'] == u'This a single line No Split test!\n'
+    cmd = ['echo', 'This a single line No Split test!']
+    assert run(cmd, split=False)['stdout'] == u'This a single line No Split test!\n'
 
 
 def test_check_multiline_output():
-    a_command = ['echo', 'This a multi-\nline test!']
-    assert run(a_command, split=True)['stdout'] == [u'This a multi-', u'line test!']
+    cmd = ['echo', 'This a multi-\nline test!']
+    assert run(cmd, split=True)['stdout'] == [u'This a multi-', u'line test!']
 
 
 def test_check_multiline_output_no_split():
-    a_command = ['echo', 'This a multi-\nline No Split test!']
-    assert run(a_command, split=False)['stdout'] == u'This a multi-\nline No Split test!\n'
+    cmd = ['echo', 'This a multi-\nline No Split test!']
+    assert run(cmd, split=False)['stdout'] == u'This a multi-\nline No Split test!\n'
 
 
 def test_check_error():
-    a_command = ['false']
+    cmd = ['false']
     with pytest.raises(CalledProcessError):
-        run(a_command, checked=True)
+        run(cmd, checked=True)
 
 
 def test_check_error_no_checked():
-    a_command = ['false']
-    assert run(a_command, checked=False)['exit_code'] == 1
+    cmd = ['false']
+    assert run(cmd, checked=False)['exit_code'] == 1
+
+
+def test_stdin_string():
+    ret = run(('bash', '-c', 'read MSG; echo "<$MSG>"'), stdin='LOREM IPSUM')
+    assert ret['stdout'] == '<LOREM IPSUM>\n'
+
+
+def test_stdin_fd():
+    r, w = os.pipe()
+    # The string we write here should not exceed `/proc/sys/fs/pipe-max-size`
+    # which represents the size of the kernel buffer backing the pipe
+    os.write(w, b'LOREM IPSUM')
+    os.close(w)
+    ret = run(('bash', '-c', 'read MSG; echo "<$MSG>"'), stdin=r)
+    os.close(r)
+    assert ret['stdout'] == '<LOREM IPSUM>\n'
+
+
+@pytest.mark.parametrize('p', _STDIN)
+def test_stdin_check(p):
+    with pytest.raises(TypeError):
+        run(('true',), stdin=p)
 
 
 def test_is_verbose(monkeypatch):
