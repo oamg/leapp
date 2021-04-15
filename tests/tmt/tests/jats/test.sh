82c3d9e9986dcd10d0797a5f5d68e6567d19b840
Inessa Vasilevskaya <ivasilev@redhat.com>
2021-04-15 09:33:29 +0200

    Resolve review comments: amahdal and pstodulk

--- tests/tmt/tests/jats/test.sh
+++ tests/tmt/tests/jats/test.sh
@@ -2,13 +2,13 @@
 
 TESTS_RESULT=test_results
 JAT_LOG="/var/tmp/jat/last/log.yaml"
-JAT_RELICS="/var/tmp/jat/last/relics/xcase-results.yaml"
+JAT_RELICS="/var/tmp/jat/last/relics"
 
 suite=${TESTSUITE:-leapp}
 testname=${TESTNAME:-cli}
 
 jats_test="/usr/share/jats/suite/$suite/$testname"
-test_dir_name=$(echo "$jats_test" | sed -e 's/\//_/g')
+test_dir_name=$(echo "$jats_test" | sed -e 's:/:_:g')
 echo "Processing $jats_test.."
 # perform cleanup just in case previous test failed hard
 if [ -d /var/tmp/jat ]; then
@@ -16,12 +16,12 @@ if [ -d /var/tmp/jat ]; then
 fi
 mkdir "$test_dir_name"
 # save overall test result
-jattool runtest $jats_test
+jattool runtest "$jats_test"
 # pack artifacts and test log
 echo "$jats_test\nResult: $?" >> "$TESTS_RESULT"
 cp "$JAT_LOG" "$test_dir_name/log.yaml"
 if [ -f "$JAT_RELICS" ]; then
-    cp "$JAT_RELICS" "$test_dir_name/xcase-results.yaml"
+    cp -ar "$JAT_RELICS" "$test_dir_name"
 fi
 jattool export "$JAT_LOG"
-mv $(ls jat_report-*) "$test_dir_name/jat_report.html"
+mv jat_report-*.html "$test_dir_name/jat_report.html"
