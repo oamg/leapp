73f298d3b8ed7d7847bef19804925d90bc53f747
Vinzenz Feenstra <vfeenstr@redhat.com>
2021-04-15 12:36:05 +0200

    cli: Automatic command discovery for leapp CLI
    This patch introduces a new approach in detecting the leapp cli commands.
    The main function will now look into the directory of leapp.cli.commands
    and list all directories within that directory and attempts to import a
    `register` function from them. If that succeeds it will call this function
    passing the toplevel command object to it allowing the function to register
    the commands and if necessary sub commands to it.
    
    This patch will also drop leapp/cli/upgrade and reintroduce it to
    leapp-repository in a different PR.
    
    Framework version will now be 2.0
    
    Signed-off-by: Vinzenz Feenstra <vfeenstr@redhat.com>

--- leapp/cli/__init__.py
+++ leapp/cli/__init__.py
@@ -1,18 +1,37 @@
+
 import os
+import pkgutil
 import socket
 
-from leapp.utils.clicmd import command, command_opt
-from leapp.cli import upgrade
 from leapp import VERSION
+from leapp.cli import commands
+from leapp.utils.clicmd import command
 
 
 @command('')
-def cli(args): # noqa; pylint: disable=unused-argument
-    pass
+def cli(args):  # noqa; pylint: disable=unused-argument
+    """
+    Top level base command dummy function
+    """
+
+
+def _load_commands(base_command):
+    pkgdir = os.path.dirname(commands.__file__)
+    for entry in os.listdir(pkgdir):
+        entry_path = os.path.join(pkgdir, entry)
+        if os.path.isdir(entry_path) and os.path.isfile(os.path.join(entry_path, '__init__.py')):
+            # We found a package - We will import it and get the `register` symbol and check if it is callable.
+            package_name = 'leapp.cli.commands.{}'.format(entry)
+            package = pkgutil.get_loader(package_name).load_module(package_name)
+            register = getattr(package, 'register', None)
+            if callable(register):
+                register(base_command)
 
 
 def main():
+    """
+    leapp entry point
+    """
     os.environ['LEAPP_HOSTNAME'] = socket.getfqdn()
-    for cmd in [upgrade.list_runs, upgrade.preupgrade, upgrade.upgrade, upgrade.answer, upgrade.rerun]:
-        cli.command.add_sub(cmd.command)
+    _load_commands(cli.command)
     cli.command.execute('leapp version {}'.format(VERSION))
