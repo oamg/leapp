d3d81b14227d18586d03377c1ea77c992f234f7e
Vinzenz Feenstra <vfeenstr@redhat.com>
2021-10-18 13:10:12 +0200

    Python 3.9 compatibility

--- leapp/utils/workarounds/mp.py
+++ leapp/utils/workarounds/mp.py
@@ -1,8 +1,18 @@
 import os
 import multiprocessing.util
+import multiprocessing
+
+_start_method_set = False
 
 
 def apply_workaround():
+    global _start_method_set
+    # Set start method for multiprocessing to fork (3.4+)
+    set_start_method = getattr(multiprocessing, 'set_start_method', None)
+    if not _start_method_set and set_start_method:
+        set_start_method('fork')
+        _start_method_set = True
+
     # Implements:
     # https://github.com/python/cpython/commit/e8a57b98ec8f2b161d4ad68ecc1433c9e3caad57
     #
