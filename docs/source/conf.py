1aed05433f49c62e9b1d4056677ee818f690783c
Matej Matuska <mmatuska@redhat.com>
2024-03-04 13:49:18 +0100

    Update sphinx to 2.4.5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -73,6 +73,7 @@ release = leapp.FULL_VERSION
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
 language = None
+# highlight_language = "python"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
