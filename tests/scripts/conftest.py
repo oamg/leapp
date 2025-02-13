bafd8e2932f28645b9a2e73a9839f47f17123b4f
Pavel Holica <pholica@redhat.com>
2025-02-13 07:56:24 +0100

    Remove pytest plugin hook
    This broke all (reasonably recent pytest installations) when snactor was
    not installed. From now on, the tests need to load the plugin explicitly
    (either in config or in conftest).
    
    Resolves: https://issues.redhat.com/browse/RHEL-74372

--- tests/scripts/conftest.py
+++ tests/scripts/conftest.py
@@ -4,3 +4,4 @@ from helpers import make_repository_dir_fixture
 # NOTE(ivasilev) Assigning a fixture generated that way to some variable is a necessary prerequisite for it to be
 # discovered by pylint
 repodir_fixture = make_repository_dir_fixture(name='repository_dir', scope='session')
+pytest_plugins = ('leapp.snactor.fixture',)
