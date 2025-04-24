f9680cce79e9d1368aa3981fe3aebde6b36eec9a
MichalHe <michal.sk.com@gmail.com>
2025-04-24 22:42:11 +0200

    cli(show-dependencies): allow filtering per actor

--- leapp/snactor/commands/show_dependencies.py
+++ leapp/snactor/commands/show_dependencies.py
@@ -6,7 +6,7 @@ from leapp.exceptions import CommandError, LeappError
 from leapp.logger import configure_logger
 from leapp.repository.scan import find_and_scan_repositories
 from leapp.snactor.context import with_snactor_context
-from leapp.utils.clicmd import command, command_arg
+from leapp.utils.clicmd import command, command_arg, command_opt
 from leapp.utils.repository import find_repository_basedir, requires_repository
 
 _LONG_DESCRIPTION = '''
@@ -19,6 +19,9 @@ The DOT language allows easy visualization of the graph.
          help='Print dependency graph of a workflow in the DOT language.',
          description=_LONG_DESCRIPTION)
 @command_arg('workflow_name')
+@command_opt('only-influencing-actor',
+             metavar='ActorName',
+             help='Show only actors that influence the given actor by producing messages')
 @requires_repository
 @with_snactor_context
 def cli(params):
@@ -35,6 +38,12 @@ def cli(params):
     if not workflow:
         raise CommandError('Could not find any workflow named "{}"'.format(params.name))
 
+    actor_of_interest = None
+    if params.only_influencing_actor:
+        actor_of_interest = repository.lookup_actor(params.only_influencing_actor)
+        if not actor_of_interest:
+            raise CommandError('Could not find any workflow named "{}"'.format(params.name))
+
     workflow_instance = workflow()
 
     # Information about message consuments and producers:
@@ -50,12 +59,34 @@ def cli(params):
             for produced_message in actor.produces:
                 dependency_graph[produced_message.__name__]['producers'].add(actor.name)
 
+    actors_to_include = set()
+    if actor_of_interest:
+        # Compute backwards reachability
+        worklist = [actor_of_interest.name]
+        while worklist:
+            explored_actor = worklist.pop(-1)
+            actors_to_include.add(explored_actor.name)
+
+            for consumed_message in explored_actor.consumes:
+                message_producer_names = dependency_graph[consumed_message.__name__]['producers']
+                for producer_name in message_producer_names:
+                    if producer_name in actors_to_include:
+                        continue
+
+                    producer = repository.lookup_actor(producer_name)
+                    worklist.append(producer)
+
     # Display the dependency graph in the DOT format. We don't want to add dependencies on another library,
     # so we do the formatting ourselves since its pretty straightforward
     dot_output_lines = ['digraph LeappDependencyGraph {']
 
     for message, message_info in dependency_graph.items():
         for producent, consument in itertools.product(message_info['producers'], message_info['consumers']):
+            # In case we show only influential actors, check that both message source and message targets
+            # are to be displayed
+            if actor_of_interest and not actors_to_include.issubset((producent, consument)):
+                continue
+
             output_line = '  "{0}" -> "{1}" [label="{2}"]'.format(producent, consument, message)
             dot_output_lines.append(output_line)
 
